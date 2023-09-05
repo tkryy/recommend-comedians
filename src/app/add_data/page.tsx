a1b99443619dd9eaba3ae6ea7e972b6e363933f1
tkryy <yutatkr@gmail.com>
2023-09-05 12:15:27 +0900

    add_dataにジャンル選択を追加

--- src/app/add_data/page.tsx
+++ src/app/add_data/page.tsx
@@ -5,6 +5,19 @@ export default function Add_data() {
       <input type="text" placeholder="芸人名を記入" className=" input input-bordered w-full max-w-xs ml-80 text-black bg-gray-300" />
       <p className="ml-80 mt-12">特徴</p>
       <textarea placeholder="芸人の特徴を記入" className="textarea textarea-bordered textarea-md w-full max-w-xs ml-80 text-black bg-gray-300" ></textarea>
+      <select className="select select-bordered w-full max-w-xs ml-80 mt-12 mb-32 text-black bg-gray-300 box-content h-20 w-30">
+        <option disabled selected>追加する芸人に最も適する<br></br>ジャンルを選択してください。</option>
+        <option>コント師多めの演技波系</option>
+        <option>純粋な漫才多め、正統派系</option>
+        <option>正統派多め、しゃべくり系</option>
+        <option>搦め手使い、漫才コント系</option>
+        <option>変な奴ら多め、尖り系</option>
+        <option>ツッコミのパワー強め、パワー系</option>
+        <option>正統派とまでは言えないけど正統派に近い、準正統派系</option>
+        <option>演技とセンスが強い、センス系</option>
+        <option>素の人間性勝負、非演技派系</option>
+        <option>両刀多め、二刀流系</option>
+      </select>
     </div>
   );
 }

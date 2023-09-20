d81797763e367d0d807b2f9f94979266bd052719
kabuki <tanuki5sanuki3@icloud.com>
2023-09-20 15:32:26 +0900

    ページ上部ナビバーのデザイン修正上部ナビバーのデザイン修正

--- src/components/Header3.tsx
+++ src/components/Header3.tsx
@@ -4,8 +4,9 @@ import Link from 'next/link'
 function Header3({ }) {
   return (
     <>
-      <div className="navbar bg-gray-800 sticky top-0 z-50">
-        <div className="navbar-start">
+      <div className="navbar bg-opacity-80 shadow-xl backdrop-filter backdrop-blur-sm bg-[#DD0000]  sticky top-0 z-50">
+    
+        <div className="navbar-start ml-6">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden text-white">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
@@ -59,10 +60,12 @@ function Header3({ }) {
 
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl text-white">タイトル</a>
+          <Link style={{ fontFamily: "Futura" }} href="/" className=" normal-case text-2xl text-white">
+            Tendon
+            </Link>
         </div>
 
-        <div className="navbar-end">
+        <div className="navbar-end mr-6">
           {/* ベルマーク */}
           <button className="btn btn-ghost btn-circle">
             <div className="indicator text-white">
@@ -91,7 +94,8 @@ function Header3({ }) {
           </div>
 
         </div>
-      </div>
+        </div>
+ 
     </>
   );
 }

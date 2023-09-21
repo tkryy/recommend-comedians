e69e4f1b7296feb6d9495eaa728826a56c4b9c8e
kabuki <tanuki5sanuki3@icloud.com>
2023-09-21 16:32:53 +0900

    データ追加ページのスライダーデザインを変更

--- src/app/add_data/page.tsx
+++ src/app/add_data/page.tsx
@@ -8,20 +8,43 @@ export default function Add_data() {
    * @param {string} rightLabel - スライダーの右側のラベル
    * @return {JSX.Element} - Tスライダーをレンダリング
    */
-  const renderRatingSliders = (leftLabel: string, rightLabel: string) => {
+  const renderRatingSliders = (
+    title: string,
+    leftLabel: string,
+    rightLabel: string
+  ) => {
     return (
-      <div className="mt-12 min-w-[400px] z-10">
-        <div className="flex justify-between">
-          <p className="text-xl">{leftLabel}</p>
-          <p className="text-xl">{rightLabel}</p>
+      <div className="mt-12 min-w-[400px] z-10 space-y-8">
+        {/* 質問のタイトル */}
+        <div className="">
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <hr className="h-[0.5px]  bg-[#D9D9D9]  rounded " />
         </div>
-        <input type="range" min={0} max={10} className="range" step="1" />
-        <div className="flex justify-between text-xs px-2">
-          {[5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5].map((value) => (
-            <span key={value}>
-              |<br /> {value}
-            </span>
-          ))}
+        {/* パラメータのスライダー */}
+        <div className=" flex items-center justify-center">
+          <div className="flex justify-between flew-2/3">
+            <p className="text-xl w-[70px]">{leftLabel}</p>
+            <div>
+              <input
+                type="range"
+                min={0}
+                max={10}
+                className="w-[400px] h-2 accent-[#262526] bg-gradient-to-r from-pink-400 to-cyan-400 rounded-lg appearance-none cursor-pointer"
+              ></input>
+              <div className="flex justify-between text-xs px-[5px]">
+                {[5, 4, 3, 2, 1, 0, 1, 2, 3, 4, 5].map((value) => (
+                  <div
+                    key={value}
+                    className="flex flex-col justify-start items-center"
+                  >
+                    <p>|</p>
+                    <p>{value}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <p className="text-xl w-[70px] text-end">{rightLabel}</p>
+          </div>
         </div>
       </div>
     );
@@ -30,36 +53,42 @@ export default function Add_data() {
   return (
     <div className="container ">
       <PageTitle title="芸人データを追加" />
-      <input
-        type="text"
-        placeholder="芸人名を記入"
-        className="input input-bordered  text-black bg-gray-300"
-      />
-      <p className="">特徴</p>
-      <textarea
-        placeholder="芸人の特徴を記入"
-        className="textarea textarea-bordered textarea-md  text-black bg-gray-300"
-      ></textarea>
+      <div className="space-y-9">
+        <h2 className="text-3xl font-bold">基本情報</h2>
+        <input
+          type="text"
+          placeholder="芸人の名前を記入"
+          className="input input-bordered  text-black bg-gray-300"
+        />
+        <p className="">特徴</p>
+        <textarea
+          placeholder="芸人の特徴を記入"
+          className="textarea textarea-bordered textarea-md  text-black bg-gray-300"
+        ></textarea>
 
-      {renderRatingSliders("精密性", "柔軟性")}
-      {renderRatingSliders("正統派", "破天荒")}
-      {/*  
+        <h2 className="text-3xl font-bold">分析データ</h2>
+        <div className="mx-12">
+          {renderRatingSliders("どんなスタイル？", "精密", "柔軟")}
+          {renderRatingSliders("笑いの取り方は", "正統派", "破天荒")}
+          {/*  
     内部データ上の分類は問題ないが、サイト上の表記としては「頭悪い」は不適切かもしれない。
     「センス・パワー」との差別化もわかりにくいので他の表現も模索する。
     */}
-      {renderRatingSliders("賢い", "頭悪い")}
+          {renderRatingSliders("ネタの印象は？", "賢い", "頭悪い")}
 
-      {/*  
+          {/*  
     以下の部分は「ワード」というパラメータがあったがデザイン的な統一を考えて省略した。
     問題があるようであれば修正を検討する。
     */}
-      {renderRatingSliders("センス", "パワー")}
+          {renderRatingSliders("強みといえば？", "センス", "パワー")}
 
-      {renderRatingSliders("演技派", "素")}
-      {renderRatingSliders("はやい", "ゆっくり")}
-      {renderRatingSliders("尖り", "真面目")}
-      {renderRatingSliders("イケメン", "ブサイク")}
-      {renderRatingSliders("人気", "コア")}
+          {renderRatingSliders("雰囲気は？", "演技派", "天然")}
+          {renderRatingSliders("ネタのテンポは？", "はやい", "ゆっくり")}
+          {renderRatingSliders("性格は？", "尖り", "真面目")}
+          {renderRatingSliders("華はある方？", "イケメン", "個性的")}
+          {renderRatingSliders("今の知名度は？", "人気", "コア")}
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -7,7 +7,7 @@ interface PageTitleProps {
 function PageTitle({ title }: PageTitleProps) {
   return (
     <div className="mb-7">
-      <h1 className="text-3xl font-bold text-[#732C02]">{title}</h1>
+      <h1 className="text-4xl font-bold text-[#732C02]">{title}</h1>
       <hr className="h-px my-8 " />
     </div>
   );

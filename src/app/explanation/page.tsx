18164b2cbae318057cc6ea4521bc7b9d8379b95e
tkryy <yutatkr@gmail.com>
2023-09-24 16:43:18 +0900

    使い方ページスマホ対応

--- src/app/explanation/page.tsx
+++ src/app/explanation/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Explanation() {
   return (
-    <div>
+    <div className="md:ml-12">
       <PageTitle title="使い方" />
-      <p className="font-bold text-xl">自分の好きな芸人名を入力すると、入力した芸人と似ている芸人をAIが判断して、おすすめを提案します！</p>
+      <p className="font-bold md:text-xl px-2">自分の好きな芸人名を入力すると、入力した芸人と似ている芸人をAIが判断して、おすすめを提案します！</p>
 
-      <div className="p-12">
-        <p className="mt-2">(例)</p>
-        <div className="flex justify-center items-center ">
+      <div className="p-8 md:p-12">
+        <p className="">(例)</p>
+        <div className="flex justify-center items-center w-[150px] md:w-[600px] ml-24 md:ml-2">
           <Image
             src={"/images/recommend_exam01.png"}
             alt="例１"
@@ -23,9 +23,8 @@ export default function Explanation() {
             height="100"
           />
         </div>
-        <p className="mt-2">左図のように、好きな芸人名を入力すると、右の図のようにおすすめの芸人が出力されます。</p>
+        <p className="mt-2 ml-4 md:ml-2 w-[300px] md:w-[600px]">左図のように好きな芸人名を入力すると、右図のようにおすすめの芸人が出力されます。</p>
       </div>
-
 
     </div>
 

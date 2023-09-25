7c7d1e7d6bf9cda64290b8b90943fcd210390cac
kabuki <tanuki5sanuki3@icloud.com>
2023-09-25 14:15:56 +0900

    ロゴ追加とHeaderデザイン修正

--- src/components/Header3.tsx
+++ src/components/Header3.tsx
@@ -1,44 +1,84 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
-function Header3({ }) {
+function Header3({}) {
   return (
     <>
-      <div className="navbar border-t-4 border-t-[#DD0000] bg-opacity-90 shadow-lg backdrop-filter backdrop-blur-sm bg-[#F28705] fixed top-0 z-50">
-    
+      <div className="navbar bg-opacity-80 shadow-lg backdrop-filter backdrop-blur-sm bg-[#F25C05] fixed top-0 z-50">
         <div className="navbar-start lg:ml-6">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost xl:hidden text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-200 rounded-box w-52 text-black">
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-gray-200 rounded-box w-52 text-black"
+            >
               <li>
                 <Link href="/">
-                  <Image alt="home" width="20" height="20" src="/icons/home_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="home"
+                    width="20"
+                    height="20"
+                    src="/icons/home_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>ホーム</p>
                 </Link>
               </li>
               <li>
                 <Link href="/recommend_comedians">
-                  <Image alt="recommend" width="20" height="20" src="/icons/smart_toy_black_24dp.svg"></Image>
+                  <Image
+                    alt="recommend"
+                    width="20"
+                    height="20"
+                    src="/icons/smart_toy_black_24dp.svg"
+                  ></Image>
                   <p>AIおすすめ診断</p>
                 </Link>
               </li>
               <li>
                 <Link href="/search_comedians">
-                  <Image alt="search" width="20" height="20" src="/icons/search_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="search"
+                    width="20"
+                    height="20"
+                    src="/icons/search_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>芸人を探す</p>
                 </Link>
               </li>
               <li>
                 <Link href="/favorite">
-                  <Image alt="star" width="20" height="20" src="/icons/star_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="star"
+                    width="20"
+                    height="20"
+                    src="/icons/star_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>お気に入り</p>
                 </Link>
               </li>
               <li>
                 <Link href="/add_data">
-                  <Image alt="add" width="20" height="20" src="/icons/person_add_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="add"
+                    width="20"
+                    height="20"
+                    src="/icons/person_add_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>データ追加</p>
                 </Link>
               </li>
@@ -46,30 +86,65 @@ function Header3({ }) {
               <div className="mt-40 border-2 border-gray-300"></div>
               <li>
                 <Link href="/explanation">
-                  <Image alt="hatena" width="20" height="20" src="/icons/help_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="hatena"
+                    width="20"
+                    height="20"
+                    src="/icons/help_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>使い方</p>
                 </Link>
-
               </li>
               <li>
                 <Link href="/setting">
-                  <Image alt="setting" width="20" height="20" src="/icons/settings_FILL0_wght400_GRAD0_opsz24.svg"></Image>
+                  <Image
+                    alt="setting"
+                    width="20"
+                    height="20"
+                    src="/icons/settings_FILL0_wght400_GRAD0_opsz24.svg"
+                  ></Image>
                   <p>設定</p>
                 </Link>
               </li>
-
             </ul>
           </div>
-          <Link style={{ fontFamily: "Futura" }} href="/" className=" normal-case text-2xl text-white font-bold">
-            Tendon
-            </Link>
+          <Link
+            style={{ fontFamily: "Futura" }}
+            href="/"
+            className=" normal-case text-2xl text-white "
+          >
+            <div className="flex items-center">
+              <Image
+                src={"/icons/tendonIcon_white.svg"}
+                alt="tendon"
+                width={50}
+                height={50}
+              ></Image>
+              {/* サービス名の文字の太さは通常でお願いします */}
+              {/* もしboldにする場合は資料などもそれに統一する必要あり */}
+              <h1 className="ml-3">Tendon</h1>
+            </div>
+          </Link>
         </div>
 
         <div className="navbar-end mr-6">
           {/* ベルマーク */}
           <button className="btn btn-ghost btn-circle">
             <div className="indicator text-white">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
               <span className="badge badge-xs badge-primary indicator-item"></span>
             </div>
           </button>
@@ -78,24 +153,34 @@ function Header3({ }) {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <Image alt="icon_mark" width="20" height="20" src="/icons/account_circle_white_24dp.svg"></Image>
+                <Image
+                  alt="icon_mark"
+                  width="20"
+                  height="20"
+                  src="/icons/account_circle_white_24dp.svg"
+                ></Image>
               </div>
             </label>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white">
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-white"
+            >
               <li>
                 <a className="justify-between">
                   Profile
                   <span className="badge">New</span>
                 </a>
               </li>
-              <li><a>Settings</a></li>
-              <li><a>Logout</a></li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
             </ul>
           </div>
-
         </div>
-        </div>
- 
+      </div>
     </>
   );
 }

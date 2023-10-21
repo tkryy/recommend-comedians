98b7ddcc7948625821202c408f77e17810eb14df
tkryy <yutatkr@gmail.com>
2023-10-21 11:27:55 +0900

    使い方セクション修正

--- src/components/landing/HowToUse.tsx
+++ src/components/landing/HowToUse.tsx
@@ -2,28 +2,26 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const HowToUse = () => {
-  return(
-    
+  return (
     <div className="flex justify-center flex-wrap">
-      <h1 className="text-5xl font-bold w-full ml-96 p-4 text-yellow-500">芸人名を入力、おすすめを提案！</h1>
-      <div className="flex justify-center space-x-5 -ml-24">
-      <Image
-          src={"/images/recommend_exam01.png"}
-          alt="診断ページの図"
+      <h1 className="text-5xl font-bold w-full ml-80 pt-8 text-yellow-500">芸人名を入力、おすすめを提案！</h1>
+      <p className="text-xl w-full ml-64 p-4">あなたの好みの芸人名を入力すると、入力した芸人と似ている芸人をAIが判断して、おすすめを提案します！</p>
+      <div className="flex justify-center space-x-5 -ml-24 ">
+        <Image
+          src={"/images/recommend_exam03.png"}
+          alt="診断ページの図1"
           width="500"
           height="320"
-          className=""
+          className="mockup-window border border-base-300"
         ></Image>
         <Image
-          src={"/images/recommend_exam02.png"}
-          alt="診断ページの図"
+          src={"/images/recommend_exam04.png"}
+          alt="診断ページの図2"
           width="500"
           height="320"
-          className=""
+          className="mockup-window border border-base-300"
         ></Image>
       </div>
-        
     </div>
-
   );
 };

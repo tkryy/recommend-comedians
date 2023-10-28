580fb7ab4e1c56a450be69fadd1d5e9c9c99d8f8
tkryy <yutatkr@gmail.com>
2023-10-28 11:57:13 +0900

    スマホ対応ver2

--- src/components/landing/HowToUse.tsx
+++ src/components/landing/HowToUse.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 export const HowToUse = () => {
   return (
     <div className="flex justify-center flex-wrap">
-      <h1 className="text-5xl font-bold w-full ml-80 pt-8 text-yellow-500">芸人名を入力、おすすめを提案！</h1>
-      <p className="text-xl w-full ml-64 p-4">あなたの好みの芸人名を入力すると、入力した芸人と似ている芸人をAIが判断して、おすすめを提案します！</p>
-      <div className="flex justify-center space-x-5 -ml-44 ">
+      <h1 className="md:text-5xl text-2xl font-bold w-full md:ml-80 ml-4 md:pt-8 pt-4 text-yellow-500">芸人名を入力、おすすめを提案！</h1>
+      <p className="md:text-xl text-base w-full md:ml-64 md:p-8 p-2 ">あなたの好みの芸人名を入力すると、入力した芸人と<br className="md:hidden"></br>似ている芸人をAIが判断して、おすすめを提案します！</p>
+      <div className="md:flex justify-center md:space-x-5 md:-ml-44 -ml-4 md:p-0 p-16 md:mt-0 -mt-12">
         <Image
           src={"/images/recommend_exam03.png"}
           alt="診断ページの図1"
           width="500"
           height="320"
-          className="mockup-window border border-base-300"
+          className="mockup-window border border-base-300 md:mb-0 mb-4"
         ></Image>
         <Image
           src={"/images/recommend_exam04.png"}
